89f46372b00345d85f1ea9d0648efdeb70d1e918
mgillon <michael.gillon@hotmail.co.uk>
2020-02-01 16:07:19 +0000

    added warning light script

---
+++ Assets/Interactive Object/Trigger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.Interactive_Object
+{
+    public class Trigger : MonoBehaviour
+    {
+
+        public UnityEvent OnEnter;
+        public UnityEvent OnExit;
+
+        void OnTriggerEnter(Collider collide)
+        {
+            if (collide.tag == "Player")
+            {
+                OnEnter.Invoke();               
+            }
+        }
+
+        void OnTriggerExit(Collider collide)
+        {
+            if (collide.tag == "Player")
+            {
+                OnExit.Invoke();
+            }
+        }
+    }
+}
