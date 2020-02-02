88a1cd8ff2932ffbb18e5edba6e7016075d2e2a0
David Strachan <davidstrachan92@gmail.com>
2020-02-02 12:57:59 +0000

    Added health script

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColor : MonoBehaviour
+{
+    public Image fill;
+    public Color maxHealthColor = Color.green;
+    public Color minHealthColor = Color.red;
+
+    private Slider _slider;
+    private Director _director;
+
+    private void Start()
+    {
+        _slider = GetComponent<Slider>();
+        _director = FindObjectOfType<Director>();
+    }
+
+    private void OnGUI()
+    {
+        var health = _director.Health;
+        _slider.value = health;
+        fill.color = Color.Lerp(minHealthColor, maxHealthColor, health);
+    }
+}
