ac45a09137b459980bebace1a975f95a90b02238
David Strachan <davidstrachan92@gmail.com>
2020-02-02 01:27:22 +0000

    Add bump to lightbulbs

---
+++ Assets/HingeBump.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HingeBump : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine("Bump");
+    }
+
+    private IEnumerator Bump()
+    {
+        yield return new WaitForSeconds(Random.value);
+        GetComponent<Rigidbody>().AddForce(new Vector3(Random.value, 0, Random.value) * 2, ForceMode.Impulse);
+    }
+}
